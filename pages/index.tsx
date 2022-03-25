8d6d68eccc36872c5c3323e3bd5b46a5cc662e1d
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-25 14:53:28 -0300

    :building_construction: search input working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import * as C from "@styles/home";
 import Image from "next/image";
 import searchIcon from "@images/search.svg";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return (
-    <C.Home>
-      <a
-        href="https://octodex.github.com/inspectocat/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          width={220}
-          height={220}
-          src="https://octodex.github.com/images/inspectocat.jpg"
-          title="octodex inspectocat"
-          alt="octodex inspectocat"
-        />
-      </a>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input placeholder="search a github user..." type="search" />
-        <button>
-          <Image src={searchIcon} width={30} height={30} alt="search icon" />
-        </button>
-      </form>
-    </C.Home>
-  );
+    const router = useRouter();
+    const [githubName, setGithubName] = useState("");
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        router.push(`/${githubName}`);
+    }
+
+    return (
+        <C.Home>
+            <a
+                href="https://octodex.github.com/inspectocat/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <Image
+                    width={220}
+                    height={220}
+                    src="https://octodex.github.com/images/inspectocat.jpg"
+                    title="octodex inspectocat"
+                    alt="octodex inspectocat"
+                />
+            </a>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="search a github user..."
+                    value={githubName}
+                    onChange={(e) => setGithubName(e.target.value)}
+                    type="search"
+                />
+                <button>
+                    <Image
+                        src={searchIcon}
+                        width={30}
+                        height={30}
+                        alt="search icon"
+                    />
+                </button>
+            </form>
+        </C.Home>
+    );
 }
