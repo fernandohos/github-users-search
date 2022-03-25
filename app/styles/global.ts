e2361ea7957b4d6e2f4e52ed904ac4ce9a52c6eb
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-25 00:14:11 -0300

    :lipstick: adding font size to global style

--- app/styles/global.ts
+++ app/styles/global.ts
@@ -5,9 +5,11 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-size: 1.6rem;
     }
 
     html {
         scroll-behavior: smooth;
+        font-size: 62.5%;
     }
 `;
