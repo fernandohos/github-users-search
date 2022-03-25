2bdf1d29767141f938a24b4750a947118328661e
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-25 00:15:54 -0300

    :construction: home page

---
+++ app/styles/home.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Home = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: -8rem;
+
+    form {
+        display: flex;
+        box-shadow: 0 4px 7px #bbb;
+        border-radius: .5rem;
+        overflow: hidden;
+        margin-top: 5rem;
+        transition: all ease .2s;
+        
+        &:focus-within, &:hover {
+            box-shadow: 0 4px 17px #bbb;
+            transform: translateY(-.2rem);
+        }
+
+        input {
+            border: none;
+            padding: 20px;
+            width: 45rem;
+            font-size: 1.8rem;
+            outline: none;
+
+            &::-webkit-search-cancel-button {
+                cursor: pointer;
+            }
+        }
+
+        button {
+            cursor: pointer;
+            border: none;
+            background: orange;
+            height: 100%;
+            aspect-ratio: 1/1;
+            outline: none;
+            transition: all ease .2s;
+
+            &:focus, &:hover {
+                box-shadow: 0 0 5px #888 inset;
+            }
+        }
+    }
+`;
